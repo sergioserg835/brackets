db41348de61e37b651b5a10f50a64e6b83df8955
sergioserg835 <sergioserg835@gmail.com>
2021-03-15 16:30:03 +0200

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let allowBrackets = [];
+            for (let i = 0; i < bracketsConfig.length; i++) {
+                allowBrackets.push(bracketsConfig[i][0]+bracketsConfig[i][1]);
+            }
+            
+            let newStr = '';
+            for (let j = 0; j < 10; j++) {
+                for (i = 0; i < allowBrackets.length; i++) {    
+                    while (str.indexOf(allowBrackets[i]) > -1) {
+                        newStr = str.replace(allowBrackets[i], '');
+                        str = newStr;
+                        newStr = '';    
+                    }
+                    if (str.length == 0) break;
+                }
+                if (str.length == 0) break;
+            }
+            if (str.length > 0) 
+                return false;
+            else
+                return true;
 }
